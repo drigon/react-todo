00e3b58690fdd96e6f00b987591cd19507ee4631
Rodrigo Castilho Ishii <drigon4ever@gmail.com>
2022-01-04 22:42:13 -0300

    TodoList

---
+++ src/components/pages/Reddit.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useQuery } from "react-query";
+
+export default function Reddit() {
+  //const { data: posts, isLoading, errorMessage } = useFetch('https://www.reddit.com/r/aww.json');
+
+  const {
+    data: posts,
+    isLoading,
+    isError,
+    error,
+    isSuccess,
+  } = useQuery("posts", fetchPosts);
+
+  function fetchPosts(params) {
+    return fetch("https://www.reddit.com/r/aww.json").then((response) =>
+      response.json()
+    );
+  }
+
+  return (
+    <div>
+      <h2>Reddit Api</h2>
+
+      {isLoading && <div>Loading...</div>}
+
+      {isSuccess && (
+        <ul>
+          {posts.data.children.map((post) => (
+            <li key={post.data.id}>
+              <a href={`https://www.reddit.com${post.data.permalink}`}>
+                {post.data.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {isError && <div>{error.message}</div>}
+    </div>
+  );
+}
